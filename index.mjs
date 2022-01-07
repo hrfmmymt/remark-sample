5d8fb192ca5b02c89baeb4cf7ed00e6c30a81cc3
hrfmmymt <hrfmmymt@gmail.com>
2022-01-07 12:26:02 +0900

    start

---
+++ index.mjs
@@ -0,0 +1,26 @@
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import rehypeSanitize from 'rehype-sanitize';
+import rehypeStringify from 'rehype-stringify';
+
+const input = `
+# h1
+- list 1
+- list 2
+
+aaaaaa
+`;
+
+async function main() {
+  const file = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeSanitize)
+    .use(rehypeStringify)
+    .process(input)
+
+  console.log(String(file))
+}
+
+main();
